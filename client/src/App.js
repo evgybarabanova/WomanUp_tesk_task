b1d9028875234801383bd89349b3470d259a5480
evgybarabanova <evgybarabanova@gmail.com>
2022-11-16 17:28:27 +0100

    add landing, page, logic

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,13 @@
-
+import { Routes, Route, Router } from 'react-router-dom';
+import Landing from './page/Landing';
 
 function App() {
   return 
-  
+<>
+<Routes>
+  <Route path='/' element={<Landing />}></Route>
+</Routes>
+</>
 }
 
 export default App;
---
+++ client/src/logic/retrieveNotes.js
@@ -0,0 +1,14 @@
+import retrieveNotes from './retrieveNotes'
+import completeNotes from './completeNotes'
+import createNotes from './createNotes'
+import deleteNotes from './deleteNotes'
+import updateNotes from './updateNotes'
+
+
+export{
+retrieveNotes,
+completeNotes,
+createNotes,
+deleteNotes,
+updateNotes
+}
