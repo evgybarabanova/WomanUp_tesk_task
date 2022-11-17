64ad4e713ac37f65df65166aa31242f3680df769
Evgy <evgybarabanova@gmail.com>
2022-11-17 23:21:01 +0300

    add handles

--- client/src/components/Notes.jsx
+++ client/src/components/Notes.jsx
@@ -1,21 +1,91 @@
 import React, { useEffect, useState } from "react";
-import { createNote, updateNote, deleteNote } from "../logic";
+import { updateNotes, deleteNotes, retrieveNotes } from "../logic";
 
 export default function Notes() {
   const [notes, setNotes] = useState([]);
 
+  useEffect(() => {
+    try {
+      retrieveNotes()
+        .then((notes) => {
+          setNotes(notes);
+        })
+        .catch((error) => alert(error.message));
+    } catch (error) {
+      alert(error.message);
+    }
+  }, []);
+
+  const handleDeleteNote = (noteId) => {
+    try {
+      deleteNotes(noteId)
+        .then(() => retrieveNotes())
+        .then((notes) => setNotes(notes))
+        .catch((error) => alert(error.message));
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
+  const handleUpdateNote = (noteId, description) => {
+    try {
+      updateNotes(noteId, description).catch((error) => alert(error.message));
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
   return (
     <ul className="list">
       <li className="list_item">
-        <div className="note">
-          <button className="note__delete-button">x</button>
-          <button className="note__update-button">update</button>
-          <button className="note__add-button">add files</button>
-          <p>Name of task</p>
-          <p>Description</p>
-          <p>Date of ending</p>
-          <p>File of note</p>
-        </div>
+        {notes.map((note) => (
+          <div className="note">
+            <button
+              className="note__delete-button"
+              onClick={() => handleDeleteNote(note.id).reverse()}
+            >
+              x
+            </button>
+            <p>add name</p>
+            <p
+              className="note__name"
+              contentEditable="true"
+              onKeyUp={(event) => {
+                const name = event.target.name;
+
+                handleUpdateNote(note.id, name);
+              }}
+            >
+              {note.name}
+            </p>
+            <p>add description</p>
+            <p
+              className="note__description"
+              contentEditable="true"
+              onKeyUp={(event) => {
+                const description = event.target.innerText;
+
+                handleUpdateNote(note.id, description);
+              }}
+            >
+              {note.description}
+            </p>
+            <p>add date</p>
+            <p
+              className="sticker__date"
+              contentEditable="true"
+              onKeyUp={(event) => {
+                const date = event.target.date;
+
+                handleUpdateNote(note.id, date);
+              }}
+            >
+              {note.date}
+            </p>
+            <p>File of note</p>
+            <button className="note__add-button">add files</button>
+          </div>
+        ))}
       </li>
     </ul>
   );
